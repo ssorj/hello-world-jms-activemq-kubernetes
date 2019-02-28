62f7cc6950c6f4f86fb8bfdc3d1ebae455b57604
Justin Ross <jross@apache.org>
2019-02-28 05:49:44 -0800

    Convert to async send and receive; clean everything up

--- receiver/src/main/java/net/example/Receiver.java
+++ receiver/src/main/java/net/example/Receiver.java
@@ -1,24 +1,22 @@
 package net.example;
 
-import io.netty.channel.Channel;
 import java.net.URI;
 import java.util.Hashtable;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import javax.inject.Singleton;
-import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
 import javax.jms.JMSException;
-import javax.jms.MessageConsumer;
+import javax.jms.Message;
+import javax.jms.MessageListener;
 import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.GET;
-import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import org.glassfish.jersey.netty.httpserver.NettyHttpContainerProvider;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
@@ -28,9 +26,34 @@ import org.slf4j.LoggerFactory;
 @Path("/api")
 public class Receiver {
     private static Logger log = LoggerFactory.getLogger(Receiver.class);
-    
+    private static ConnectionFactory connectionFactory = null;
+
     public static void main(String[] args) {
         try {
+            // AMQP
+
+            String amqpHost = System.getenv("MESSAGING_SERVICE_HOST");
+            String amqpPort = System.getenv("MESSAGING_SERVICE_PORT");
+            String user = System.getenv("MESSAGING_SERVICE_USER");
+            String password = System.getenv("MESSAGING_SERVICE_PASSWORD");
+
+            if (amqpHost == null) amqpHost = "localhost";
+            if (amqpPort == null) amqpPort = "5672";
+            if (user == null) user = "example";
+            if (password == null) password = "example";
+
+            String url = String.format("failover:(amqp://%s:%s)", amqpHost, amqpPort);
+
+            Hashtable<Object, Object> env = new Hashtable<Object, Object>();
+            env.put("connectionfactory.factory1", url);
+
+            InitialContext context = new InitialContext(env);
+            ConnectionFactory factory = (ConnectionFactory) context.lookup("factory1");
+
+            Receiver.connectionFactory = factory;
+
+            // HTTP
+
             String host = System.getenv("HTTP_HOST");
             String port = System.getenv("HTTP_PORT");
 
@@ -42,76 +65,56 @@ public class Receiver {
 
             NettyHttpContainerProvider.createHttp2Server(uri, rc, null);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Startup failed", e);
             System.exit(1);
         }
     }
 
-    MessageConsumer consumer;
+    private JMSContext jmsContext = Receiver.connectionFactory.createContext();
+    private ConcurrentLinkedQueue<String> strings = new ConcurrentLinkedQueue<>();
 
-    public Receiver() throws NamingException, JMSException {
-        String host = System.getenv("MESSAGING_SERVICE_HOST");
-        String port = System.getenv("MESSAGING_SERVICE_PORT");
-        String user = System.getenv("MESSAGING_SERVICE_USER");
-        String password = System.getenv("MESSAGING_SERVICE_PASSWORD");
+    public Receiver() {
+        synchronized (jmsContext) {
+            Queue queue = jmsContext.createQueue("example/strings");
+            JMSConsumer consumer = jmsContext.createConsumer(queue);
 
-        if (host == null) host = "localhost";
-        if (port == null) port = "5672";
-        if (user == null) user = "example";
-        if (password == null) password = "example";
+            consumer.setMessageListener(new ReceiveListener());
+        }
+    }
 
-        String url = String.format("failover:(amqp://%s:%s)", host, port);
-        String address = "example/strings";
+    class ReceiveListener implements MessageListener {
+        @Override
+        public void onMessage(Message message) {
+            String string;
 
-        Hashtable<Object, Object> env = new Hashtable<Object, Object>();
-        env.put("connectionfactory.factory1", url);
+            try {
+                string = message.getBody(String.class);
+            } catch (JMSException e) {
+                log.error("Message access error", e);
+                return;
+            }
 
-        InitialContext context = new InitialContext(env);
-        ConnectionFactory factory = (ConnectionFactory) context.lookup("factory1");
-        Connection conn = factory.createConnection(user, password);
+            log.info("RECEIVER: Received message '{}'", string);
 
-        log.info("RECEIVER: Connecting to '{}'", url);
-
-        conn.start();
-
-        synchronized (this) {
-            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Queue queue = session.createQueue(address);
-            consumer = session.createConsumer(queue);
+            strings.add(string);
         }
     }
 
     @POST
     @Path("/receive")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces("text/plain")
     public String receive() {
-        TextMessage message;
-        String text;
+        log.info("RECEIVER: Receive endpoint invoked");
 
-        try {
-            synchronized (this) {
-                message = (TextMessage) consumer.receiveNoWait();
-
-                if (message == null) {
-                    return null;
-                }
-
-                text = message.getText();
-            }
-        } catch (JMSException e) {
-            e.printStackTrace();
-            throw new InternalServerErrorException(e);
-        }
-
-        log.info("RECEIVER: Received message '{}'", text);
-
-        return text;
+        return strings.poll();
     }
 
     @GET
-    @Path("/health")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String health() {
+    @Path("/ready")
+    @Produces("text/plain")
+    public String ready() {
+        log.info("RECEIVER: Readiness checked");
+
         return "OK\n";
     }
 }
--- sender/src/main/java/net/example/Sender.java
+++ sender/src/main/java/net/example/Sender.java
@@ -1,26 +1,23 @@
 package net.example;
 
-import io.netty.channel.Channel;
 import java.net.URI;
 import java.util.Hashtable;
+import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.jms.Connection;
+import javax.jms.CompletionListener;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
 import javax.jms.JMSException;
-import javax.jms.MessageProducer;
+import javax.jms.JMSProducer;
+import javax.jms.Message;
 import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import org.glassfish.jersey.netty.httpserver.NettyHttpContainerProvider;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
@@ -30,86 +27,91 @@ import org.slf4j.LoggerFactory;
 @Path("/api")
 public class Sender {
     private static Logger log = LoggerFactory.getLogger(Sender.class);
+    private static ConnectionFactory connectionFactory = null;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try {
-            String host = System.getenv("HTTP_HOST");
-            String port = System.getenv("HTTP_PORT");
+            // AMQP
 
-            if (host == null) host = "0.0.0.0";
-            if (port == null) port = "8080";
+            String amqpHost = System.getenv("MESSAGING_SERVICE_HOST");
+            String amqpPort = System.getenv("MESSAGING_SERVICE_PORT");
+            String user = System.getenv("MESSAGING_SERVICE_USER");
+            String password = System.getenv("MESSAGING_SERVICE_PASSWORD");
 
-            URI uri = URI.create(String.format("http://%s:%s/", host, port));
+            if (amqpHost == null) amqpHost = "localhost";
+            if (amqpPort == null) amqpPort = "5672";
+            if (user == null) user = "example";
+            if (password == null) password = "example";
+
+            String url = String.format("failover:(amqp://%s:%s)", amqpHost, amqpPort);
+
+            Hashtable<Object, Object> env = new Hashtable<Object, Object>();
+            env.put("connectionfactory.factory1", url);
+
+            InitialContext context = new InitialContext(env);
+            ConnectionFactory factory = (ConnectionFactory) context.lookup("factory1");
+
+            Sender.connectionFactory = factory;
+
+            // HTTP
+
+            String httpHost = System.getenv("HTTP_HOST");
+            String httpPort = System.getenv("HTTP_PORT");
+
+            if (httpHost == null) httpHost = "0.0.0.0";
+            if (httpPort == null) httpPort = "8080";
+
+            URI uri = URI.create(String.format("http://%s:%s/", httpHost, httpPort));
             ResourceConfig rc = new ResourceConfig(Sender.class);
 
             NettyHttpContainerProvider.createHttp2Server(uri, rc, null);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Startup failed", e);
             System.exit(1);
         }
     }
 
-    Session session;
-    MessageProducer producer;
-
-    public Sender() throws NamingException, JMSException {
-        String host = System.getenv("MESSAGING_SERVICE_HOST");
-        String port = System.getenv("MESSAGING_SERVICE_PORT");
-        String user = System.getenv("MESSAGING_SERVICE_USER");
-        String password = System.getenv("MESSAGING_SERVICE_PASSWORD");
-
-        if (host == null) host = "localhost";
-        if (port == null) port = "5672";
-        if (user == null) user = "example";
-        if (password == null) password = "example";
-
-        String url = String.format("failover:(amqp://%s:%s)", host, port);
-        String address = "example/strings";
-
-        Hashtable<Object, Object> env = new Hashtable<Object, Object>();
-        env.put("connectionfactory.factory1", url);
-
-        InitialContext context = new InitialContext(env);
-        ConnectionFactory factory = (ConnectionFactory) context.lookup("factory1");
-        Connection conn = factory.createConnection(user, password);
-
-        log.info("SENDER: Connecting to '{}'", url);
-
-        conn.start();
-
-        synchronized (this) {
-            session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Queue queue = session.createQueue(address);
-            producer = session.createProducer(queue);
-        }
-    }
+    private JMSContext jmsContext = Sender.connectionFactory.createContext();
 
     @POST
     @Path("/send")
-    @Consumes(MediaType.TEXT_PLAIN)
-    @Produces(MediaType.TEXT_PLAIN)
-    public String send(String text) {
-        TextMessage message;
+    @Consumes("text/plain")
+    @Produces("text/plain")
+    public String send(String string) {
+        synchronized (jmsContext) {
+            Queue queue = jmsContext.createQueue("example/strings");
+            JMSProducer producer = jmsContext.createProducer();
 
-        try {
-            synchronized (this) {
-                message = session.createTextMessage(text);
-                producer.send(message);
-            }
-        } catch (JMSException e) {
-            e.printStackTrace();
-            throw new InternalServerErrorException(e);
+            producer.setAsync(new SendListener()).send(queue, string);
         }
 
-        log.info("SENDER: Sent message '{}'", text);
+        log.info("SENDER: Sent message '{}'", string);
 
         return "OK\n";
     }
 
+    class SendListener implements CompletionListener {
+        @Override
+        public void onCompletion(Message message) {
+            try {
+                log.info("SENDER: Receiver acknowledged '{}'", message.getBody(String.class));
+            } catch (JMSException e) {
+                log.error("Message access error", e);
+            }
+        }
+
+        @Override
+        public void onException(Message message, Exception e) {
+            log.info("SENDER: Send failed: {}", e.toString());
+        }
+    }
+
     @GET
-    @Path("/health")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String health() {
+    @Path("/ready")
+    @Produces("text/plain")
+    public String ready() {
+        log.info("SENDER: Readiness checked");
+
         return "OK\n";
     }
 }
